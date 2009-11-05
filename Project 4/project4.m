d4cefa7cb301f6e5ce1e1ce68808bb17fba19100
Christoph Koehler <christoph@zerodeviation.net>
2009-11-05 12:19:44 -0600

    Add initial project code.

---
+++ Project 4/project4.m
@@ -0,0 +1,41 @@
+% ==========
+% = Part 4 =
+% ==========
+
+% desired azimuth
+iaz = 130; 
+
+% desired range
+ir = 300; 
+% Number of points for DFT (zero-padding)
+npts = 64;
+
+if ~exist('X_h','var') & ~exist('X_v','var') 
+	fprintf('No data to process.\n'); 
+end
+X=X_h;
+% % extract time series data
+% 
+time_series = squeeze(X(iaz,ir,:)); 
+time = [1:length(time_series)]*pri; 
+va = lambda/4/pri; 
+
+% neg for vf fd neg relationship 
+vel=-[-npts/2:(npts/2)-1]*2*va/npts; 
+
+% % data window % 
+d=boxcar(num_pulses); 
+% calculate psd % 
+S=periodogramse(time_series,d,npts); 
+S=(pri*abs(fftshift(S))); 
+
+% plot time data I/Q
+plot(time, real(time_series), time, imag(time_series));
+
+% % plot % 
+figure;
+plot(vel,S); 
+grid on; 
+xlabel('Radial Velocity (m/s)'); 
+ylabel('S(f) (arbitrary power units)'); 
+title('Periodogram');
